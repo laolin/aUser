0d41c5ff3caca26c0de8590616cffe5adfa6aa50
LaoLin <hi@laolin.com>
2015-02-27 02:07:09 +0800

    db config

--- src/index.php
+++ src/index.php
@@ -1,11 +1,12 @@
 <?php
-
-if( isset(LAOLIN_CFG['PATH'] ))
-  CFG_PATH=LAOLIN_CFG['PATH'];
+if( isset($LAOLIN_CFG['PATH'] ))
+  $CFG_PATH=$LAOLIN_CFG['PATH'];
 else 
-  CFG_PATH='.';
-if( file_exists(CFG_PATH '/db.config.php' ))
-  require CFG_PATH '/db.config.php';
+  $CFG_PATH='.';
+  
+
+if( file_exists($CFG_PATH . '/db.config.php' ))
+  require ($CFG_PATH . '/db.config.php');
 require_once 'medoo/class.laolindb.php';
 
 
