b35cbe936f9e1001ceae99f3a4201837577c049b
LaoLin <hi@laolin.com>
2015-02-27 19:58:45 +0800

    temp page to use Toro

--- src/index.php
+++ src/index.php
@@ -9,29 +9,57 @@ if( file_exists($CFG_PATH . '/db.config.php' ))
   require ($CFG_PATH . '/db.config.php');
 require_once 'medoo/class.laolindb.php';
 
+require_once 'Toro/Toro.php';
+
 
 srand();
-$dt=Date('Y-m-d H:i:s');
-$db=new laolinDb();
+$GLOBALS['db']=new laolinDb();
+
+$dt="B3," . rand() . ' - ' . Date('Y-m-d H:i:s');
+echo "<h2> $dt </h2><pre>";
 
 
+//WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+class SplashHandler{
+    function get() {
+        echo "Hello, world";
+    }
+}
+class ProductHandler {
+    function get($name) {
+      echo "You want to see product: $name";
+      if(!$name)$name='%';
+      $res=$GLOBALS['db']->select("book",'*',['id[~]'=>$name]);
+      var_dump($res);
+    }
+    
+    
+    function abc(){
+    
+      $db->insert("book", [
+        "title" => "$dt",
+        'rating'=>rand()%10,
+        "time" => rand(),
+        'price'=>rand()%100
+      ]);
 
-$db->insert("book", [
-	"title" => "Hi61! $dt",
-  'rating'=>rand()%10,
-	"time" => rand(),
-  'price'=>rand()%100
+      $db->update("book",['price'=>rand()%10000/100,'rating'=>rand()%10],
+        ['id[~]'=>"%".(rand()%10)]
+      );
+
+      // */
+
+      //$db->delete("book", ['time[<]'=>55555] );
+
+    }
+}
+
+Toro::serve([
+    "/hello" => "SplashHandler",
+    "/list/([a-zA-Z0-9-_]+)" => "ProductHandler",
+    "/list" => "ProductHandler"
 ]);
 
-$db->update("book",['price'=>rand()%10000/100,'rating'=>rand()%10],
-  ['id[~]'=>"%".(rand()%10)]
-);
+//WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 
-// */
 
-//$db->delete("book", ['time[<]'=>55555] );
-
-echo "<h2>61 @ $dt </h2>";
-echo '<pre>';
-
-var_dump($db);
